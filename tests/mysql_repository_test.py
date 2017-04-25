10f27b13df357e2d817b30cb2a127aae83f58ffe
Matteo Vaccari <vaccari@pobox.com>
2017-04-25 16:24:46 +0200

    we can now query the database

--- tests/mysql_repository_test.py
+++ tests/mysql_repository_test.py
@@ -17,13 +17,23 @@ class MysqlRepositoryTest(unittest.TestCase):
 
 
     def test_connect(self):
-        pymysql.connect(
+        connection = pymysql.connect(
             db=os.environ['MYSQL_DATABASE_DB'],
             host=os.environ['MYSQL_DATABASE_HOST'],
             user=os.environ['MYSQL_DATABASE_USER'],
-            password=os.environ['MYSQL_DATABASE_PASSWORD']
+            password=os.environ['MYSQL_DATABASE_PASSWORD'],
+            cursorclass=pymysql.cursors.DictCursor
             )
-        pass
+        with connection.cursor() as cursor:
+            sql = 'select 3 + 4 as sum'
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            self.assertEqual(7, result['sum'])
+
+            sql = "select concat(%s, %s, %s) as concatenation"
+            cursor.execute(sql, ('Dear ', 'Mr. ', "O'Malley"))
+            result = cursor.fetchone()
+            self.assertEqual("Dear Mr. O'Malley", result['concatenation'])
 
 
 
