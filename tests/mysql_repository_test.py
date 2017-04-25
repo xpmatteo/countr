76d4a45c97b7a474020cfa5b4b38f94770c16b80
Matteo Vaccari <vaccari@pobox.com>
2017-04-25 17:44:44 +0200

    simplified repository test

--- tests/mysql_repository_test.py
+++ tests/mysql_repository_test.py
@@ -45,41 +45,26 @@ class MysqlRepositoryTest(unittest.TestCase):
         os.environ['MYSQL_DATABASE_USER'] = 'countr'
         os.environ['MYSQL_DATABASE_PASSWORD'] = 'countr'
 
-        self.connection = pymysql.connect(
+        connection = pymysql.connect(
             db=os.environ['MYSQL_DATABASE_DB'],
             host=os.environ['MYSQL_DATABASE_HOST'],
             user=os.environ['MYSQL_DATABASE_USER'],
             password=os.environ['MYSQL_DATABASE_PASSWORD'],
             cursorclass=pymysql.cursors.DictCursor
             )
-        self.cursor = self.connection.cursor()
-        self.cursor.execute('truncate table counts')
-        self.connection.commit()
+        cursor =connection.cursor()
+        cursor.execute('truncate table counts')
+        cursor.execute("insert into counts (id, value) values ('123', 999)")
+        connection.commit()
+        connection.close()
+        
         self.repository = MysqlCountRepository()
 
-    def tearDown(self):
-        self.connection.close()
-
-    def test_connect(self):
-        sql = 'select 3 + 4 as sum'
-        self.cursor.execute(sql)
-        result = self.cursor.fetchone()
-        self.assertEqual(7, result['sum'])
-
-        sql = "select concat(%s, %s, %s) as concatenation"
-        self.cursor.execute(sql, ('Dear ', 'Mr. ', "O'Malley"))
-        result = self.cursor.fetchone()
-        self.assertEqual("Dear Mr. O'Malley", result['concatenation'])
-
     def test_read_one_counter(self):
-        self.cursor.execute("insert into counts (id, value) values ('123', 999)")
-        self.connection.commit()
         self.assertEqual(999, self.repository['123'])
         self.assertTrue(999, self.repository['123'])
 
     def test_count_exists_test(self):
-        self.cursor.execute("insert into counts (id, value) values ('123', 999)")
-        self.connection.commit()
         self.assertTrue('123' in self.repository)
         self.assertFalse('222' in self.repository)
 
